7e24abe9d1ae17d67bc5ed9f2e87c6d5b4b8fe2b
Edwin574 <owinoedwin574@gmail.com>
2022-08-22 11:38:17 +0300

    positioned the buttons

--- scripts/app.js
+++ scripts/app.js
@@ -3,33 +3,24 @@ const button = document.getElementById("check-button");
 const feedback = document.getElementById("feedback");
 
 button.addEventListener("click", function (e) {
-  e.preventDefault();
-  let inputValue = myInput.value.toLowerCase();
-  let reverseString = inputValue.split("").reverse().join("");
+    e.preventDefault();
+    let inputValue = myInput.value.toLowerCase();
+    let reverseString = inputValue.split("").reverse().join("");
 
     if (inputValue == "") {
-      alert("Enter a a word in the input");
+        alert("Enter a a word in the input");
     }
-  if (inputValue == reverseString && inputValue !== "") {
-    feedback.innerHTML = "This is a palindrome";
-    console.log("palindrome");
-  } else if (inputValue !== reverseString && inputValue !== "") {
-    feedback.innerHTML = "This is not a palindrome";
-    console.log("not palindrome");
-  }
+    if (inputValue == reverseString && inputValue !== "") {
+        feedback.textContent = `${myInput.value} is a palindrome`
+        feedback.classList.add('success')
+        feedback.classList.remove('fail')
+
+        console.log("palindrome");
+    } else if (inputValue !== reverseString && inputValue !== "") {
+        feedback.textContent = `${myInput.value} is not a palindrome`;
+        feedback.classList.add('fail')
+        feedback.classList.remove('success')
+      
+        console.log("not palindrome");
+    }
 });
-
-// const checkPalindrome = () => {
-//     input.addEventListener('input', function () {
-//         let inputValue =(input.value).toString()
-//         if (inputValue === inputValue.reverse()) {
-//              feedback.innerHTML=(`${inputValue} is a Palindrome`)
-//         } else {
-//             feedback.innerHTML=(`${inputValue} is not Palindrome`)
-//          }
-//      })
-
-// }
-// button.addEventListener('click', function () {
-//     checkPalindrome()
-// })
